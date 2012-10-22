7418b76e88be6fc37fb5844e6095c386f18a75e5
Ben West <bewest@gmail.com>
2012-10-22 12:12:55 -0700

    Toy hexdump decoder, clean working area.
    If you have scapy,
    
        python decoder.py -vvvvv pcaps/first_run/hexlify.csv
    
    Produces output like this:
    ```bash
    $ decoder.py -vvvvv pcaps/first_run/hexlify.csv
    INFO:decoder:opening ['pcaps/first_run/hexlify.csv']
    out
    0000   04 00                                              ..
    None
    in
    0000   01 55 00 0B 2C 00 00 00  00 43 6F 6D 4C 69 6E 6B   .U..,....ComLink
    0010   20 49 49 01 10 02 00 01  01 03 00 00 00 00 00 00    II.............
    0020   00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00   ................
    [...]
    out
    0000   05 01 00                                           ...
    None
    in
    0000   01 55 00 00 00 00 00 00  00 01 71 00 00 01 71 01   .U........q...q.
    0010   74 02 7C 01 71 04 00 00  05 00 0A 00 00 00 00 00   t.|.q...........
    0020   00 01 01 55 00 00 00 00  00 00 00 00 00 00 00 00   ...U............
    0030   00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00   ................
    None
    ```

--- decoder.py
+++ decoder.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
+"""
+decoder.py - An analysis of Medtronic Carelink Diabetes protocol using
+scapy.
+"""
 
+# stdlib
 import user
 import sys
+import argparse
+import logging
+import binascii
+
+logging.basicConfig(stream=sys.stdout)
+logger = logging.getLogger('decoder')
+logger.setLevel(logging.DEBUG)
+
+# Requires scapy to be in your PYATHONPATH
 #import scapy
 from scapy.all import *
+from scapy import utils
 
 _usb_response = { 0x00: '', 'U': "Success", 'f': "Fail" }
 
@@ -16,10 +31,76 @@ class USBPacket(Packet):
     XByteField("error", 0),
   ]
 
-def main(args):
-  print "hello"
+class Decoder(object):
+  def __init__(self, path, opts):
+    self.path = path
+    self.opts = opts
+
+  def __call__(self):
+    self.open( )
+    self.decode( )
+    self.close( )
+
+  def open(self):
+    self.handle = None
+    if self.path == '-':
+      self.handle = sys.stdin
+    else:
+      self.handle = open(self.path, 'rU')
+
+  def close(self):
+    if self.path != '-':
+      self.handle.close( )
+
+  def decode(self):
+    for line in self.handle.readlines( ):
+      line = line.strip( )
+      method, data = line.split(',')
+      data = binascii.unhexlify(data)
+      print method
+      print utils.hexdump(data)
+
+
+
+class Console:
+  _log_map = { 0: logging.ERROR, 1: logging.WARN,
+               2: logging.INFO, 3: logging.DEBUG }
+  def __init__(self, args):
+    self.raw_args = args
+    self.parser = self.get_argparser( )
+    args = list(args)
+    cmd, args = args[0], args[1:]
+    self.opts = self.parser.parse_args((args))
+    logger.setLevel(self._log_map.get(self.opts.verbose, 3))
+    cmdverbose = ''
+    if self.opts.verbose > 0:
+      cmdverbose = '-' + ('v' * self.opts.verbose)
+      
+    #logger.info('opts: %s' % (pformat(args)))
+    cmdline = [ cmd, cmdverbose ] + self.opts.input
+    print ' '.join(cmdline)
+
+  def main(self):
+    logger.info('opening %s' % (self.opts.input))
+
+    for item in self.opts.input:
+      self.do_input(item)
+
+  def do_input(self, item):
+    decode = Decoder(item, self.opts)
+    print decode( )
+
+  def get_argparser(self):
+    """Prepare an argument parser."""
+    parser = argparse.ArgumentParser(description=__doc__ )
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help="Verbosity.")
+    parser.add_argument('input', nargs='+', help="Input files")
+    return parser
 
 if __name__ == '__main__':
-  main(list(sys.argv))
+  app = Console(sys.argv)
+  app.main( )
+
 #####
 # EOF
