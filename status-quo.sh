98155b1cb2c8f69a799b5efc40a5a0515305fdaf
Ben West <bewest@gmail.com>
2013-01-06 15:38:36 -0800

    capture CRC errors

--- status-quo.sh
+++ status-quo.sh
@@ -9,6 +9,10 @@ export TIME="%C\n\telapsed %E\n\tuser %U\n\tsystem %S\n\tCPU %P (%Xtext+%Ddata %
   time python pump/stick.py /dev/ttyUSB0
   time python pump/session.py /dev/ttyUSB0 208850
   time python pump/commands.py /dev/ttyUSB0 208850
+  echo "Was there an ACK ERROR?"
+  echo "### DIAGNOSE CRC"
+  time python pump/stick.py /dev/ttyUSB0
+  time python pump/stick.py /dev/ttyUSB0
 ) 2>&1 ) 2>&1 | tee status-quo.log
 
 #####
