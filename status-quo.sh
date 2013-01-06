1d276303d87499db4cb8a72e37ecc2891e08ecfd
Ben West <bewest@gmail.com>
2013-01-06 14:32:31 -0800

    another clean run

--- status-quo.sh
+++ status-quo.sh
@@ -1,12 +1,15 @@
 #/bin/bash
 # make an example of an interesting log worth sharing
 
+TIME="%C\n\telapsed %E\n\tuser %U\n\tsystem %S\n\tCPU %P (%Xtext+%Ddata %Mmax)k"
+
 ( cat $0
   date
+  time (
   time python pump/stick.py /dev/ttyUSB0
   time python pump/session.py /dev/ttyUSB0 208850
   time python pump/commands.py /dev/ttyUSB0 208850
-) 2>&1 | tee status-quo.log
+) 2>&1 ) 2>&1 | tee status-quo.log
 
 #####
 # EOF
