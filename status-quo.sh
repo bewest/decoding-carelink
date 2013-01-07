a17c9a4bda8a055395725a46214a33a88aff1d0e
Ben West <bewest@gmail.com>
2013-01-06 16:45:11 -0800

    another clean run

--- status-quo.sh
+++ status-quo.sh
@@ -5,11 +5,14 @@ export TIME="%C\n\telapsed %E\n\tuser %U\n\tsystem %S\n\tCPU %P (%Xtext+%Ddata %
 
 SERIAL=${1-'208850'}
 CMD="$*"
+NAME=$0
 
-( cat $0
-  echo $CMD
+( cat $NAME
+  echo $NAME $CMD
   date
+  export TIME="%C\n\telapsed %E\n\tuser %U\n\tsystem %S\n\tCPU %P (%Xtext+%Ddata %Mmax)k"
   time (
+  export TIME="%C\n\telapsed %E\n\tuser %U\n\tsystem %S\n\tCPU %P (%Xtext+%Ddata %Mmax)k"
   time python pump/stick.py /dev/ttyUSB0
   time python pump/session.py /dev/ttyUSB0 ${SERIAL}
   time python pump/commands.py /dev/ttyUSB0 ${SERIAL}
