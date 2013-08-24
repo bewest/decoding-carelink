c9f9546f7853f2cd07aee67edb22f7905dfb4952
Ben West <bewest@gmail.com>
2013-08-24 16:47:25 -0700

    fix for xiali

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,10 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
+  'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath',
+  'sphinx.ext.mathjax', 'sphinx.ext.viewcode'
+  ] + ['sphinxcontrib.seqdiag']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
