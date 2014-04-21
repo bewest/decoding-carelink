47ed1a7b024c31e874884ff7238582a9650a60f3
Ben West <bewest@gmail.com>
2014-04-21 08:36:42 -0700

    decode basal profiles

--- decocare/commands.py
+++ decocare/commands.py
@@ -833,7 +833,52 @@ class ReadSettings512 (PumpCommand):
   code = 145
 # MMPump512/	CMD_READ_STD_PROFILES	146	0x92	('\x92')	??
 class ReadProfile_STD512 (PumpCommand):
+  """
+    >>> import json
+    >>> schedule = ReadProfile_STD512.decode(ReadProfile_STD512._test_result_1)
+    >>> len(schedule)
+    4
+    >>> print json.dumps(schedule[0])
+    {"start": "00:00:00", "rate": 0.8}
+    >>> print json.dumps(schedule[1])
+    {"start": "06:30:00", "rate": 0.9500000000000001}
+    >>> print json.dumps(schedule[2])
+    {"start": "09:30:00", "rate": 1.1}
+    >>> print json.dumps(schedule[3])
+    {"start": "14:00:00", "rate": 0.9500000000000001}
+
+  """
+  _test_result_1 = bytearray([
+    32, 0, 0,
+    38, 0, 13,
+    44, 0, 19,
+    38, 0, 28,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
+  ])
+  _test_schedule = {'total': 22.50, 'schedule': [
+    { 'start': '12:00A', 'rate': 0.80 },
+    { 'start': '6:30A', 'rate': 0.95 },
+    { 'start': '9:30A', 'rate': 1.10 },
+    { 'start': '2:00P', 'rate': 0.95 },
+  ]}
   code = 146
+  @staticmethod
+  def decode (data):
+    i = 0
+    schedule = [ ]
+    end = [ 0, 0, 0 ]
+    none = [ 0, 0, 0x3F ]
+    for i in xrange(len(data)/3):
+      off = i*3
+      r, z, m = data[off : off + 3]
+      if [r,z,m] in [end, none]:
+        break
+      schedule.append(dict(start=str(lib.basal_time(m)), rate=r*.025))
+    return schedule
+  def getData (self):
+    return self.decode(self.data)
 # MMPump512/	CMD_READ_A_PROFILES	147	0x93	('\x93')	OK
 class ReadProfile_A512 (PumpCommand):
   code = 147
