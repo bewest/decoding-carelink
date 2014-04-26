43b41f5b6db4e66d54a293affcd296a9e562d640
Edward Robinson <edward.r.robinson@gmail.com>
2014-04-26 12:27:36 +0000

    did a bunch of downloads (see analyse/2014-04-26 folder for data and a readme.txt of what I did.

--- download-cgm-18.sh
+++ download-cgm-18.sh
@@ -1,5 +1,10 @@
 
+#for ((i=0; i<=4; i++ ))
+#do
+
 dt=$(date +%Y%m%d_%H%M%S)
-sudo ./bin/mm-send-comm.py --init --prefix-path logs/$dt- --serial 584923 --port /dev/ttyUSB0 tweak ReadGlucoseHistory --page 16 --save | tee analysis/pg-16-$dt-ReadGlucoseHistory.markdown
-sudo ./bin/mm-send-comm.py --prefix-path logs/$dt- --serial 584923 --port /dev/ttyUSB0 tweak ReadISIGHistory --page 16 --save | tee analysis/pg-16-$dt-ReadISIGHistory.markdown
+sudo ./bin/mm-send-comm.py --prefix-path logs/$dt- --serial 584923 --port /dev/ttyUSB0 tweak ReadGlucoseHistory --page 17 --save | tee analysis/pg-17-$dt-ReadGlucoseHistory.markdown
+sudo ./bin/mm-send-comm.py --prefix-path logs/$dt- --serial 584923 --port /dev/ttyUSB0 tweak ReadISIGHistory --page 17 --save | tee analysis/pg-17-$dt-ReadISIGHistory.markdown
+#sleep 300
+#done
 
