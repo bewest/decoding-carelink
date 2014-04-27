ce2e23f24ef5052c490882ad33a2f946c1a57eb9
Edward Robinson <edward.r.robinson@gmail.com>
2014-04-27 03:15:38 +0000

    tried the write history timestamp command and stuck the results in the take5 directory

---
+++ write-bg-history.sh
@@ -0,0 +1,5 @@
+
+
+dt=$(date +%Y%m%d_%H%M%S)
+sudo ./bin/mm-send-comm.py --init --prefix-path logs/$dt- --serial 584923 --port /dev/ttyUSB0 tweak WriteGlucoseHistoryTimestamp --save | tee analysis/write-$dt-WriteGlucoseHistoryTimestamp.markdown
+
