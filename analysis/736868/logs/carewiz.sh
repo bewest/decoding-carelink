871b71f4b3fe6947b4afdd6428040a2209bf1fbb
Ben West <bewest@gmail.com>
2015-05-06 14:26:05 -0700

    add tool fmt wizards nicely from carelink csv

---
+++ analysis/736868/logs/carewiz.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mm-pretty-csv ../CareLink-Export-1427693806171.csv  |  grep Wizard |
+  tr -d '"' | tr -s ' ' |
+  while IFS=', ' read timestamp name details bg_units  bg_input carb_input carb_units carb_ratio sensitivity bg_target_low bg_target_high bolus_estimate correction_estimate food_estimate unabsorbed_total trash_1 trash_2 junk ; do
+    if [[ -n $junk ]] ; then
+      echo -n $timestamp $name ''
+      # $details $bg_input $bg_units $carb_input
+      export $details
+      echo -n $BG_INPUT ''
+      # BG_UNITS
+      # export "$bg_units/$bg_input"
+      export $carb_input
+      export $carb_units
+      export $carb_ratio
+      export $sensitivity
+      export $bg_target_low
+      export $bg_target_high
+      export $bolus_estimate
+      export $correction_estimate
+      export $food_estimate
+      export $unabsorbed_total
+
+      echo -n $CARB_INPUT $CARB_$RATIO ''
+      echo -n $INSULIN_SENSITIVITY ''
+      echo -n $BG_TARGET_LOW $BG_TARGET_HIGH ''
+      echo -n $BOLUS_ESTIMATE $CORRECTION_ESTIMATE $FOOD_ESTIMATE $UNABSORBED_INSULIN_TOTAL
+      echo
+      # echo 'xx' $sensitivity bglo $bg_target_low bghi $bg_target_high
+    fi
+  done
+
+#
+# for x in ReadHistory*.json ; do cat $x |  json -a timestamp _type bg carb_input carb_ratio sensitivity bg_target_low bg_target_high bolus_estimate correction_estimate food_estimate unabsorbed_insulin_total _head _date _body ; done | grep Wiz | sort | tee wizards.tsv
+#
+
