d254fd625a1c3ba769a78503c196e4b7120a4312
Ben West <bewest@gmail.com>
2015-05-10 17:40:45 -0700

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
         return f.read()
 
 setup(name='decocare',
-    version='0.0.10', # http://semver.org/
+    version='0.0.11', # http://semver.org/
     description='Audit, inspect, and command MM insulin pumps.',
     long_description=readme(),
     author="Ben West",
