e12f18aee54c6af51135d99d482bff268a2c4485
Chris Hannemann <chris.hannemann@gmail.com>
2015-07-14 20:34:50 -0700

    Model 523 and 723 read_reservoir
    Added specific command call for x23 models for read_reservoir

--- decocare/models/__init__.py
+++ decocare/models/__init__.py
@@ -223,6 +223,7 @@ class Model523 (Model522):
   strokes_per_unit = 40
   larger = True
   read_carb_ratios = Task(commands.ReadCarbRatios)
+  read_reservoir = Task(commands.ReadRemainingInsulin523)
 
 class Model530 (Model523):
   pass
